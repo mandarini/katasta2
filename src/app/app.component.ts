51e3c9084e8007ffdc58c9310177638f35431250
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-12-28 16:52:36 +0200

    copying over components and services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterModule, FormsModule, CommonModule],
+  selector: 'katastasis-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'katasta2';
+  title = 'katastasis';
+  userIsLoggedIn$ = this.authService.userIsLoggedIn();
+  password = '';
+
+  constructor(private authService: AuthenticationService) {}
+
+  login() {
+    this.authService.login(this.password);
+    this.userIsLoggedIn$ = this.authService.userIsLoggedIn();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { guestGuard } from './guest.guard';
+import { MainComponent } from './contents/main/main.component';
+import { AccommodationComponent } from './contents/accommodation/accommodation.component';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  {
+    path: 'home',
+    component: AccommodationComponent,
+  },
+  {
+    path: 'wedding',
+    component: MainComponent,
+    canActivate: [guestGuard],
+    children: [
+      {
+        path: 'accommodation',
+        component: AccommodationComponent,
+      },
+    ],
+  },
+];

4135c467119f09b1f1df7fe6b9bb41a7a46e0d4c
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-12-29 10:19:07 +0200

    change firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,5 +10,5 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "katastasis-wed", "appId": "1:643885718313:web:f5ac353293a28c7ffcb538", "storageBucket": "katastasis-wed.appspot.com", "apiKey": "AIzaSyDN5pe_z-DdIMhfUEi7BBGcS20yWgT5Ny0", "authDomain": "katastasis-wed.firebaseapp.com", "messagingSenderId": "643885718313", "measurementId": "G-N4LCMGGYEV" }))), importProvidersFrom(provideFirestore(() => getFirestore())), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    })]
+    }), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"test-ng-deeplinks","appId":"1:866608855731:web:6619112924493578e8cec6","storageBucket":"test-ng-deeplinks.appspot.com","apiKey":"AIzaSyC2DUwIgKawNnmggb2voE1XM-P1rpbgtrw","authDomain":"test-ng-deeplinks.firebaseapp.com","messagingSenderId":"866608855731","measurementId":"G-96JBGZ1GB1"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
